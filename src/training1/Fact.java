3f5e2abe86b1b8995b5e61ad911e0af8ce7bf5e3
Maissa <mamedaissi-ext@airfrance.fr>
2023-03-15 21:02:46 +0100

    commit maissa

---
+++ src/training1/Fact.java
@@ -0,0 +1,19 @@
+package training1;
+
+import java.util.Scanner;
+
+public class Fact {
+	public static void main(String[] args) {
+		System.out.print(" saisir un nombre entier:");
+		Scanner sc = new Scanner(System.in);
+		String value = sc.nextLine();
+		int n = Integer.valueOf(value);
+		int i = 1;
+		int result = 1;
+		while (i <= n) {
+			result = result * i;
+			i++;
+		}
+		System.out.print("La factoriel de " + value + " est " + result);
+	}
+}
