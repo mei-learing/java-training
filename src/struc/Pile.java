9fe6676281b7584d9a9bb170c59868ea3d3611de
Maissa <mamedaissi-ext@airfrance.fr>
2023-03-31 12:51:33 +0200

    exercice pile

--- src/struc/Pile.java
+++ src/struc/Pile.java
@@ -1,43 +1,82 @@
 package struc;
 
- public class Pile {
+public class Pile {
+//
+//	/*
+//	 * Une pile est une liste chaînée d'informations dans laquelle : Un élément ne
+//	 * peut être ajouté qu'au sommet de la pile, 
+//	 * Un élément ne peut être retiré que du sommet de la pile. 
+//	 * Opérations autorisées empiler, toujours au sommet, 
+//	 * et
+//	 * jusqu’à la limite de la mémoire, dépiler, toujours au sommet, si la pile
+//	 * n’est pas vide . Afficher les elements de
+//	 * la pile
+//	 * 
+//	 */
 
-	/*
-	 * Une pile est une liste chaînée d'informations dans laquelle : Un élément ne
-	 * peut être ajouté qu'au sommet de la pile, 
-	 * Un élément ne peut être retiré que du sommet de la pile. 
-	 * Opérations autorisées empiler, toujours au sommet, 
-	 * et
-	 * jusqu’à la limite de la mémoire, dépiler, toujours au sommet, si la pile
-	 * n’est pas vide . Afficher les elements de
-	 * la pile
-	 * 
-	 */
-
-	private String [] elements;
+	private String[] elements;
 	private int tailleMax;
-	
-	void empiler (String value) {
-		//TODO
+	private int sommet;
+
+	public Pile(int tailleMax) {
+		// TODO Auto-generated constructor stub
+		this.tailleMax = tailleMax;
+		elements = new String[tailleMax];
+		sommet = -1;
 	}
-	
-	String depiler () {
-		//TODO
-		return null;
+
+	void empiler(String value) {
+
+		if (estPleine()) {
+			System.out.println("Impossible d'empiler, la pile est pleine");
+			return;
+		}
+
+		sommet = sommet + 1;
+
+		elements[sommet] = value;
+
 	}
-	
-	/**vérifier si la pile est vide ou non
-	 * 
-	 * @return
-	 */
-	boolean estVide () {
-		//TODO
-		return true;
+
+	String depiler() {
+		if (estVide()) {
+			System.out.println("[]");
+			return "";
+		}
+		String s= elements[sommet];
+		elements[sommet]=null;
+		sommet --;
+		return s;
 	}
-	
-	void afficher () {
-		
+
+	String sommetPile() {
+		return elements[sommet];
 	}
-	
+
+	boolean estVide() {
+		if (sommet == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	boolean estPleine() {
+		if (sommet == tailleMax - 1) {
+			return true;
+		}
+		return false;
+	}
+
+	void afficher() {
+		if (estVide()) {
+			System.out.println("Impossible d'afficher, la pile est vide");
+			return;
+		}
+
+		for (int i = sommet; i >= 0; i--) {
+			System.out.println(elements[i]);
+		}
+	}
 }
 
+//
--- src/struc/PileTest.java
+++ src/struc/PileTest.java
@@ -2,14 +2,18 @@ package struc;
 
 public class PileTest {
 	
-	public static void main(String [] args) {
-		Pile p = new Pile();
-		p.afficher(); // ==> Vide
-		p.depiler(); // Pas possible car la pile est vide
-		p.empiler("value1");
-		p.empiler("value2");
-		p.empiler("value3");
-		p.afficher();// value3 value2 value1
-		p.depiler(); // Pas possible car la pile est vide
+	public static void main(String [] args) throws Exception {
+		Pile pVide = new Pile(4);
+		System.out.println("Test d'une pile vide");
+		pVide.afficher(); // ==> Vide
+		pVide.depiler(); // Pas possible car la pile est vide
+		System.out.println("Test d'une pile pleine");
+		Pile pPleine = new Pile(4);
+		pPleine.empiler("value1");
+		pPleine.empiler("value2");
+		pPleine.empiler("value3");
+		pPleine.afficher();// value3 value2 value1
+		pPleine.depiler(); // valeur2, valeur1
+		pPleine.afficher();
 	}
 }
